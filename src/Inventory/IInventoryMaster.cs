4145f683f59c12fa9190ff8a61da40597cefa5e7
Michał Żołdak <michalzoldak97@gmail.com>
2023-05-15 22:20:51 +0200

    inventory refractor init

--- src/Inventory/IInventoryMaster.cs
+++ src/Inventory/IInventoryMaster.cs
@@ -1,23 +1,13 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace U3.Inventory
 {
     public interface IInventoryMaster
     {
-        public Dictionary<Transform, InventoryItem> Items { get; set; }
-
-        public GameObject ItemCamera {get; set;}
-
+        // events
         public void CallEventAddItem(Transform item);
+        public void CallEventItemAdded(Transform item);
         public void CallEventRemoveItem(Transform item);
-
-        public void CallEventItemAdded();
-        public void CallEventItemRemoved();
-
-        public void CallEventSelectItem(Transform item);
-        public void CallEventDeselectItem(Transform item);
-        public void CallEventItemSelected(Transform item);
-        public void CallEventItemDeselected(Transform item);
+        public void CallEventItemRemoved(Transform item);
     }
 }

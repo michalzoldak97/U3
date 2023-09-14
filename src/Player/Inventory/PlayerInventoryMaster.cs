a369b8213ec7e3ab73278b242eecae951cf9e378
michalzoldak97 <77213930+michalzoldak97@users.noreply.github.com>
2023-09-14 14:16:38 +0200

    more slot prototyping

--- src/Player/Inventory/PlayerInventoryMaster.cs
+++ src/Player/Inventory/PlayerInventoryMaster.cs
@@ -6,15 +6,18 @@ namespace U3.Player.Inventory
     public class PlayerInventoryMaster : InventoryMaster
     {
         [SerializeField] private Transform itemContainer;
-
+        // slot can be selectable or not, can have different item types accepted
+        // IInventorySlot (?) 
+        // container should have label, should be selectable or not
         // set active slot
         // add to slot (check if has been on another slot, if yes clear)
         public delegate void InventoryContainerEventHandler(Transform item, int[] idx);
-        public event InventoryContainerEventHandler EventAssignItemToSlot;
+        public event InventoryContainerEventHandler EventSetActiveContainer; // unselect previous container and slot, select the 1st selectable slot with item attached 
+        public event InventoryContainerEventHandler EventSetActiveSlot; // if item on slot select it
+        public event InventoryContainerEventHandler EventAssignItemToSlot; // if item assigned to other remove it from other, if -1 -1 (its moved back) or inactive -> deselect it, if moved on active slot select it 
         public event InventoryContainerEventHandler EventItemAssignedToSlot;
 
         public delegate void InventoryUIEventHandler();
-        public event InventoryUIEventHandler EventSetActiveSlot; // if item on slot select it
         public event InventoryUIEventHandler EventInventoryScreenOpened;
         public event InventoryUIEventHandler EventInventoryScreenClosed;
 

3b5f3d1258ad97ab780a56d2027a786be08f0291
michalzoldak97 <77213930+michalzoldak97@users.noreply.github.com>
2024-03-22 17:38:37 +0100

    CameraManager prototype

---
+++ src/Global/Rendering/CameraManager.cs
@@ -0,0 +1,3 @@
+// has an array of dependable rendere objects
+// has an array of scene cameras
+// when enabling camera it disables the rest and enables / disables related features by codes saved in scene camera

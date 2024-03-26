5d05e7fc8686bca1feba150eac7386b9ef578ae2
Michał Żołdak <michalzoldak97@gmail.com>
2024-03-27 00:24:48 +0100

    prevent obj state proxy check when app quitting

---
+++ src/Global/GameScene/SceneSwitcher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace U3.Global.GameScene
+{
+    public class SceneSwitcher : MonoBehaviour
+    {
+        public void SwotchScene(int sceneIndex)
+        {
+            SceneManager.LoadSceneAsync(sceneIndex);
+        }
+    }
+}
---
+++ src/Global/Helper/ApplicationState.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace U3.Global.Helper
+{
+    public static class ApplicationState
+    {
+        #region Static
+
+        public static bool IsQuitting { get; private set; }
+
+        public static bool IsSceneUnloading { get; private set; }
+
+#if UNITY_EDITOR
+        [InitializeOnEnterPlayMode]
+        private static void EnterPlayMode(EnterPlayModeOptions options)
+        {
+            IsQuitting = false;
+            IsSceneUnloading = false;
+        }
+#endif
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void RunOnStart()
+        {
+            IsQuitting = false;
+            IsSceneUnloading = false;
+
+            Application.quitting += Quit;
+            SceneManager.activeSceneChanged += UnloadScene;
+        }
+
+        private static void Quit()
+        {
+            IsQuitting = true;
+        }
+
+        private static void UnloadScene(Scene a, Scene b)
+        {
+            IsSceneUnloading = true;
+        }
+
+        #endregion
+    }
+}
